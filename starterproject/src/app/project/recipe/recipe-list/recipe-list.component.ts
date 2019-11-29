5985ce5b6bfb1f5361a304b7675f43f83e91cbce
vikash <vikashrajput519@gmail.com>
2019-11-29 09:12:33 +0530

    Project design

--- starterproject/src/app/project/recipe/recipe-list/recipe-list.component.ts
+++ starterproject/src/app/project/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recepe } from 'src/app/model/recipe.model';
 
 @Component({
     selector: 'recipe-list-app',
@@ -8,6 +9,13 @@ import { Component, OnInit } from '@angular/core';
 
 export class RecipeListComponent implements OnInit{
 
+    recipes : Recepe [] = [
+        new Recepe('Mutton Tandoor','A deloicious Indian Mutton tandoor','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
+        new Recepe('Prons Bhujiya','A deloicious Prons item','https://cdn.pixabay.com/photo/2018/10/31/12/37/healthy-food-3785722_960_720.jpg'),
+        new Recepe('Prons Bhujiya','A deloicious Prons item','https://cdn.pixabay.com/photo/2018/10/31/12/37/healthy-food-3785722_960_720.jpg'),
+        new Recepe('Prons Bhujiya','A deloicious Prons item','https://cdn.pixabay.com/photo/2018/10/31/12/37/healthy-food-3785722_960_720.jpg')
+    ];
+    
     ngOnInit()
     {
 
--- starterproject/src/app/project/recipe/recipe.component.ts
+++ starterproject/src/app/project/recipe/recipe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recepe } from 'src/app/model/recipe.model';
 
 @Component({
 
