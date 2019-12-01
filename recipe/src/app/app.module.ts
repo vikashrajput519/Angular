4403f6dc2d17e377b3513e2262fef0a8b02fa6ca
vikash <vikashrajput519@gmail.com>
2019-12-01 09:09:46 +0530

    After modification

--- recipe/src/app/app.module.ts
+++ recipe/src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ReceipeComponent } from './recipe/recipe.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
+const appRoutes: Routes = [
+  { path : '', component : HomeComponent },
+  { path : 'receipe', component : ReceipeComponent },
+  { path : 'receipeDetail', component : RecipeDetailComponent },
+  { path : 'receipeList', component : RecipeListComponent },
+  { path : 'receipeItems', component : RecipeItemComponent },
+  { path : 'receipeItems', component : RecipeItemComponent },
+  { path : 'shoppingList', component : ShoppingListComponent },
+  { path : 'shoppingEdit', component : ShoppingEditComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ReceipeComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- starterproject/src/app/app.module.ts
+++ starterproject/src/app/app.module.ts
@@ -5,29 +5,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BasicComponent } from './basic/basic.component';
-import { from } from 'rxjs';
 import { HomeComponent } from './home/home.component';
 import { BasicProjectComponent } from './basicproject/basicproject.component';
-import { HeaderComponent } from '../app/project/header/header.component';
-import { ReceipeComponent } from '../app/project/recipe/recipe.component';
-import { RecipeDetailComponent } from '../app/project/recipe/recipe-detail/recipe-detail.component';
-import { RecipeListComponent } from '../app/project/recipe/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from '../app/project/recipe/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './project/shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './project/shopping-list/shopping-edit/shopping-edit.component';
 import { Debugging } from './debugging/debugging.component';
 
 const appRoutes: Routes = [
   { path : '', component : HomeComponent },
   { path : 'basic', component : BasicComponent },
   { path : 'basicProject', component : BasicProjectComponent },
-  { path : 'receipe', component : ReceipeComponent },
-  { path : 'receipeDetail', component : RecipeDetailComponent },
-  { path : 'receipeList', component : RecipeListComponent },
-  { path : 'receipeItems', component : RecipeItemComponent },
-  { path : 'receipeItems', component : RecipeItemComponent },
-  { path : 'shoppingList', component : ShoppingListComponent },
-  { path : 'shoppingEdit', component : ShoppingEditComponent },
   { path : 'debug', component : Debugging },
 ];
 
@@ -37,13 +22,6 @@ const appRoutes: Routes = [
     BasicComponent,
     HomeComponent,
     BasicProjectComponent,
-    HeaderComponent,
-    ReceipeComponent,
-    RecipeDetailComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     Debugging
   ],
   imports: [
