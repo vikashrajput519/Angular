0ea6aadf839bde9ffb90cc748104a6787241091f
vikash <vikashrajput519@gmail.com>
2019-12-01 08:40:29 +0530

    New Project

--- starterproject/src/app/debugging/debugging.component.ts
+++ starterproject/src/app/debugging/debugging.component.ts
@@ -1,4 +1,5 @@
 import { OnInit, Component } from '@angular/core';
+import { Identifiers } from '@angular/compiler';
 
 @Component({
     selector : 'debugging-app',
@@ -6,7 +7,24 @@ import { OnInit, Component } from '@angular/core';
     styleUrls : ['./debugging.component.css']
 })
 export class Debugging implements OnInit{
-
+    servers = [];
+    index : number = 1;
+    
+    onClickObject()
+    {
+        this.servers.push('Server Added '+this.index);
+        this.index = this.index + 1;
+        console.log(this.index);
+       // this.index ++;
+    }
+    
+    removeOnclick( id : number)
+    {
+        //const position  = id + 1;
+        this.servers.splice(id,1);
+        this.index = this.index - 1;
+        console.log(this.index);
+    }
     ngOnInit()
     {
 
