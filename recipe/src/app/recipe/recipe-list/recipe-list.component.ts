b98d7472a88ae9d3e7c0c6684a708e6ca9839aa0
vikash <vikashrajput519@gmail.com>
2019-12-04 11:52:33 +0530

    UI changes

--- recipe/src/app/recipe/recipe-list/recipe-list.component.ts
+++ recipe/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -23,8 +23,9 @@ export class RecipeListComponent implements OnInit {
 
     ngOnInit() {
         this.recipes.push(new Recipe('Mutton Tandoor','A deloicious Indian Mutton tandoor','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'));
+        this.recipes.push(new Recipe('Prons salad', 'Helathy Prons salad', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'));
         this.recipes.push(new Recipe('Prons Bhujiya', 'A delicious Prons item', 'https://cdn.pixabay.com/photo/2018/10/31/12/37/healthy-food-3785722_960_720.jpg'));
-        this.recipes.push(new Recipe('Prons Bhujiya', 'A delicious Prons item', 'https://cdn.pixabay.com/photo/2018/10/31/12/37/healthy-food-3785722_960_720.jpg'));
+        this.recipes.push(new Recipe('Fish Fry','Delicious Fish Fry','https://p1.pxfuel.com/preview/949/796/952/food-recipe-delicious-breakfast-lunch-dinner.jpg'));
     }
 
     addNewRecipeButtonClick() {
@@ -36,6 +37,12 @@ export class RecipeListComponent implements OnInit {
         this.recipes.push(
             new Recipe(this.recipeName, this.description, this.imagePath)
         );
+    }
+    clear()
+    {
+        this.recipeName = '';
+        this.description = '';
+        this.imagePath = '';
     }
 
     listSelectedItemEmited(recipe : Recipe)
